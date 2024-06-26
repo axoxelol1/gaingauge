69c3e9312cc296ceb5b6d3a0a13506bbd3e3270a
Axel Larsson <axoxelol_01@hotmail.com>
2024-06-26 02:28:48 +0200

    Initial backend setup

--- backend/main.go
+++ backend/main.go
@@ -1,81 +1,86 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 	"os"
-    "context"
 
-	"github.com/jackc/pgx/v5"
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/joho/godotenv"
 )
 
-// album represents data about a record album.
-type album struct {
-	ID     string  `json:"id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
+type Env struct {
+	db *pgxpool.Pool
 }
 
-// albums slice to seed record album data.
-var albums = []album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+type User struct {
+	id         int64
+	First_name string `json:"first_name"`
+	Last_name  string `json:"last_name"`
 }
 
 func main() {
-	router := gin.Default()
-	router.GET("/albums", getAlbums)
-	router.GET("/albums/:id", getAlbumByID)
-	router.POST("/albums", postAlbums)
-
 	godotenv.Load()
 
-	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	conn, err := pgxpool.New(context.Background(), os.Getenv("DATABASE_URL"))
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
 		os.Exit(1)
 	}
-	defer conn.Close(context.Background())
+	defer conn.Close()
+
+	env := &Env{db: conn}
+
+	router := gin.Default()
+	router.GET("/users", env.getUsers)
 
 	router.Run("localhost:8080")
 }
 
-// getAlbums responds with the list of all albums as JSON.
-func getAlbums(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, albums)
-}
+func (e *Env) getUsers(c *gin.Context) {
 
-// postAlbums adds an album from JSON received in the request body.
-func postAlbums(c *gin.Context) {
-	var newAlbum album
-
-	// Call BindJSON to bind the received JSON to
-	// newAlbum.
-	if err := c.BindJSON(&newAlbum); err != nil {
-		return
+	rows, err := e.db.Query(context.Background(), "select * from users")
+	if err != nil {
+		c.AbortWithStatus(500)
 	}
-
-	// Add the new album to the slice.
-	albums = append(albums, newAlbum)
-	c.IndentedJSON(http.StatusCreated, newAlbum)
-}
-
-// getAlbumByID locates the album whose ID value matches the id
-// parameter sent by the client, then returns that album as a response.
-func getAlbumByID(c *gin.Context) {
-	id := c.Param("id")
-
-	// Loop through the list of albums, looking for
-	// an album whose ID value matches the parameter.
-	for _, a := range albums {
-		if a.ID == id {
-			c.IndentedJSON(http.StatusOK, a)
-			return
+	var users []User
+	for rows.Next() {
+		var user User
+		if err := rows.Scan(&user.id, &user.First_name, &user.Last_name); err != nil {
+			c.AbortWithStatus(500)
 		}
+		users = append(users, user)
 	}
-	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+
+	fmt.Println(len(users))
+	c.JSON(http.StatusOK, users)
 }
+
+// func getAlbums(c *gin.Context) {
+// 	c.IndentedJSON(http.StatusOK, albums)
+// }
+//
+// func postAlbums(c *gin.Context) {
+// 	var newAlbum album
+//
+// 	if err := c.BindJSON(&newAlbum); err != nil {
+// 		return
+// 	}
+//
+// 	albums = append(albums, newAlbum)
+// 	c.IndentedJSON(http.StatusCreated, newAlbum)
+// }
+//
+// func getAlbumByID(c *gin.Context) {
+// 	id := c.Param("id")
+//
+// 	for _, a := range albums {
+// 		if a.ID == id {
+// 			c.IndentedJSON(http.StatusOK, a)
+// 			return
+// 		}
+// 	}
+// 	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+// }
